776d428ea4e16b85b34573b6b2d9ffe2519fc7da
doblil <105515915+doblil@users.noreply.github.com>
2024-07-11 23:17:19 +0300

    init

---
+++ assets/project-VfVQOLPg.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-71OzkM9N.js";const r=c=>e.jsxs("div",{className:`project__column project__w${c.w}`,children:[e.jsx("div",{className:"project__img",children:e.jsx("img",{src:`../../public/images/${c.project}/${c.src}`,alt:c.project})}),e.jsx("div",{className:"project__sub",children:c.sub})]});export{r as P};
